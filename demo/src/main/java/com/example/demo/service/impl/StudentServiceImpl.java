038d8222192df455e773092ee62990a823c80289
x33778899 <67639693+x33778899@users.noreply.github.com>
2021-08-15 10:14:00 +0800

    Delete demo directory

--- demo/src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service.impl;
-
-import org.springframework.stereotype.Service;
-
-import com.example.demo.service.StudentService;
-@Service
-public class StudentServiceImpl implements StudentService{
-
-}
--- demo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.service.UserService;
-
-public class UserServiceImpl implements UserService{
-
-}
